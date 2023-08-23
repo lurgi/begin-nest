7845cdc983444c405d04164fa0443dc0603fb7f7
lurgi <wahoo9040@naver.com>
2023-08-23 21:22:44 +0900

    0823

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //decorater가 없는 object를 거릅니다. 즉 걸러서 저장한다.
+      forbidNonWhitelisted: true, //whiteList에 없는 object가 하나라도 있으면 그 데이터 자체를 거르고 httpException을 던집니다.
+      transform: true, //원하는 타입으로 자동으로 변환해준다.
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -9,36 +9,35 @@ import {
 } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
   constructor(private readonly movieService: MoviesService) {}
 
   @Get()
-  getAll(): Movie[] {
+  getAll(@Req() req, @Res() res): Movie[] {
     return this.movieService.getAll();
   }
 
   @Get(':id')
-  getOne(@Param('id') movieId: string): Movie {
+  getOne(@Param('id') movieId: number): Movie {
     return this.movieService.getOne(movieId);
   }
 
   @Post()
-  create(@Body() movieData): boolean {
+  create(@Body() movieData: CreateMovieDto) {
     return this.movieService.create(movieData);
   }
 
   @Delete(':id')
-  delete(@Param('id') movieId: string): boolean {
+  delete(@Param('id') movieId: number): boolean {
     return this.movieService.deleteOne(movieId);
   }
   //Put은 모든 데이터를 업데이트하고, Patch는 일부만 업데이트 한다.
   @Patch(':id')
-  patch(@Param('id') movieId: string, @Body() updateData) {
-    return {
-      updatedMovie: movieId,
-      ...updateData,
-    };
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+    return this.movieService.update(movieId, updateData);
   }
 }
