e6ab276c2b4da900394ef8544efc7e0c4cdca13a
lurgi <wahoo9040@naver.com>
2023-08-24 12:29:23 +0900

    0824 e2e unittest

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('MoviesService', () => {
   let service: MoviesService;
@@ -10,9 +11,80 @@ describe('MoviesService', () => {
     }).compile();
 
     service = module.get<MoviesService>(MoviesService);
+    service.create({
+      title: 'Test Movie',
+      genres: ['test'],
+      year: 2023,
+    });
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a movie', () => {
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+
+    it('should be throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+  describe('deleteOne', () => {
+    it('delete movie', () => {
+      const allMoviesLen = service.getAll().length;
+      service.deleteOne(1);
+      const afterDeleteLen = service.getAll().length;
+      expect(afterDeleteLen).toEqual(allMoviesLen - 1);
+    });
+    it('should return a 404', () => {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('should create a movie', () => {
+      const befroreCreate = service.getAll().length;
+      service.create({
+        title: 'Test Movie2',
+        genres: ['test'],
+        year: 2023,
+      });
+      const afterCraate = service.getAll().length;
+      expect(afterCraate).toBeGreaterThan(befroreCreate);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.update(1, { title: 'Update Text' });
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('Update Text');
+    });
+    it('should return a 404', () => {
+      try {
+        service.update(999, { title: 'works?' });
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
   });
 });
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true, //decorater가 없는 object를 거릅니다. 즉 걸러서 저장한다.
+        forbidNonWhitelisted: true, //whiteList에 없는 object가 하나라도 있으면 그 데이터 자체를 거르고 httpException을 던집니다.
+        transform: true, //원하는 타입으로 자동으로 변환해준다.
+      }),
+    );
     await app.init();
   });
 
@@ -19,6 +26,42 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcom to my Mobie API');
+  });
+
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+    });
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2023,
+          genres: ['test'],
+        })
+        .expect(201);
+    });
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
+  });
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    });
+    it('PATCH', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Updated Test' })
+        .expect(200);
+    });
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
   });
 });
